27ad175124da307cd190bb46f54ba150884681b1
Sergio del Amo <sergio.delamo@softamo.com>
2019-12-13 10:00:50 +0100

    add swagger annotations

--- src/main/java/greachconf/telegrambot/Application.java
+++ src/main/java/greachconf/telegrambot/Application.java
@@ -1,7 +1,20 @@
 package greachconf.telegrambot;
 
 import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "GreachconfBot",
+                version = "0.1",
+                description = "GreachconfBot API",
+                contact = @Contact(url = "https://greachconf.com", name = "Sergio del Amo", email = "delamos@greachconf.com")
+        )
+)
 public class Application {
 
     public static void main(String[] args) {
