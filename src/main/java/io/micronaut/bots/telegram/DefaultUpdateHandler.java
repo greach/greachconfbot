3449548b10b13d98e15310c6479e1ef5369cf488
Sergio del Amo <sergio.delamo@softamo.com>
2020-01-28 12:53:00 +0100

    echo update

--- src/main/java/io/micronaut/bots/telegram/DefaultUpdateHandler.java
+++ src/main/java/io/micronaut/bots/telegram/DefaultUpdateHandler.java
@@ -50,8 +50,8 @@ public class DefaultUpdateHandler implements UpdateHandler {
         if (applicationContext.containsBean(TelegramBot.class, Qualifiers.byName(configuration.getName()))) {
             TelegramBot telegramBot = applicationContext.getBean(TelegramBot.class, Qualifiers.byName(configuration.getName()));
 
-//            SendMessage echo = echoUpdateMessage(objectMapper, update);
-//            telegramBot.sendMessage(echo).blockingGet();
+            SendMessage echo = echoUpdateMessage(objectMapper, update);
+            telegramBot.sendMessage(echo).blockingGet();
             if (update.getCallbackQuery() !=null) {
                 String text = update.getCallbackQuery().getData();
                 if (text != null) {
